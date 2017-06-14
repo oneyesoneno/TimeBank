3de69c793be8dafab89e6eba48d3b8f5e29521eb
Isaac Ibiapina <isaac1@mail.usf.edu>
2017-06-14 13:02:31 -0400

    add custom style

--- browser/App.jsx
+++ browser/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Route , Switch} from 'react-router'
 
 import Homepage from './components/Homepage'
@@ -7,7 +8,33 @@ import SomePage from './components/SomePage'
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
+import {spacing} from 'material-ui/styles/spacing'
+import {cyan700, red500, grey600, fullWhite} from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator'
+
 injectTapEventPlugin()
+
+const muiTheme = getMuiTheme({
+  spacing: spacing,
+  fontFamily: 'Roboto, sans-serif',
+  borderRadius: 2,
+  palette: {
+    primary1Color: cyan700,
+    primary2Color: cyan700,
+    primary3Color: grey600,
+    accent1Color: '#CC181E',
+    accent2Color: '#CC181E',
+    accent3Color: '#CC181E',
+    textColor: fullWhite,
+    secondaryTextColor: fade(fullWhite, 0.7),
+    alternateTextColor: '#303030',
+    canvasColor: '#303030',
+    borderColor: fade(fullWhite, 0.3),
+    disabledColor: fade(fullWhite, 0.3),
+    pickerHeaderColor: fade(fullWhite, 0.12),
+    clockCircleColor: fade(fullWhite, 0.12),
+  },
+});
 
 export default class App extends React.Component{
 
@@ -15,7 +42,7 @@ export default class App extends React.Component{
   render() {
 
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
         <div>
         <Switch>
         <Route exact path="/" component={Homepage} />
--- browser/components/Homepage.jsx
+++ browser/components/Homepage.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import AppBar from 'material-ui/AppBar'
-import FlatButton from 'material-ui/FlatButton';
+import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import { Link } from 'react-router-dom';
+
 
 const AppBarExampleIcon = () => (
   <AppBar
     title="I wrote this!1"
-    iconElementRight={<Link to="app22"><FlatButton label="DJ Live"/></Link>}
+    iconElementLeft={<Dropdown/>}
   />
 );
 
 export default AppBarExampleIcon;
+
+
+const Dropdown = (props) => (
+  <IconMenu
+    {...props}
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <Link to='/app22'><MenuItem primaryText="How To"/></Link>
+    <Link to='/app22'><MenuItem primaryText="About"/></Link>
+  </IconMenu>
+);
