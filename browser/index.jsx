729b5b1d470c710f103437d5b13dd3b2d71557a5
Isaac Ibiapina <isaac1@mail.usf.edu>
2017-06-13 23:01:58 -0400

    initial commit

---
+++ browser/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+ReactDOM.render(
+<div>Hello world!</div>,
+  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+module.exports = {
+  entry: './browser/index.jsx',
+  output: {
+    path: __dirname,
+    filename: './bundle.js' // assumes your bundle.js will be in the root of your project folder
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '*'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015'] // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new LiveReloadPlugin({ appendScriptTag: true }),
+  ],
+};
